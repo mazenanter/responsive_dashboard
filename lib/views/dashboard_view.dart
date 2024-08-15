c182e44abc8ef0d0383258b973b3aca39bd1684e
MaZen <MazenAnter>
2024-08-16 01:17:12 +0300

    add styles to app

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dashboard/widgets/dashboard_web_layout.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: AdaptiveLayoutWidget(
+        mobileLayout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        webLayout: (context) => const DashboardWebLayout(),
+      ),
+    );
   }
 }
---
+++ lib/widgets/dashboard_web_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashboardWebLayout extends StatelessWidget {
+  const DashboardWebLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
