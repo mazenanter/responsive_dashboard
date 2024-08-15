3b94f76823317fad2bd515dfddec82d083f24014
MaZen <MazenAnter>
2024-08-15 05:11:17 +0300

    add adaptive layout widget

---
+++ lib/widgets/adaptive_layout_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveLayoutWidget extends StatelessWidget {
+  const AdaptiveLayoutWidget(
+      {super.key,
+      required this.mobileLayout,
+      required this.tabletLayout,
+      required this.webLayout});
+  final WidgetBuilder mobileLayout, tabletLayout, webLayout;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return mobileLayout(context);
+        } else if (constraints.maxWidth < 900) {
+          return tabletLayout(context);
+        } else {
+          return webLayout(context);
+        }
+      },
+    );
+  }
+}
