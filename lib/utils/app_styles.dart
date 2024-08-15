265c3f4e8653458a12ce9370f3d793d1bdacbe87
MaZen <MazenAnter>
2024-08-16 01:22:54 +0300

    add font

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const TextStyle styleRegular16 = TextStyle(
+    color: Color(0xff064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle styleMedium16 = TextStyle(
+    color: Color(0xff064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle styleSemiBold16 = TextStyle(
+    color: Color(0xff064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle styleSemiBold20 = TextStyle(
+    color: Color(0xff064060),
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Montserrat',
+  );
+  static const TextStyle styleRegular12 = TextStyle(
+    color: Color(0xffAAAAAA),
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montserrat',
+  );
+  static const TextStyle styleSemiBold24 = TextStyle(
+    color: Color(0xff4EB7F2),
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Montserrat',
+  );
+  static const TextStyle styleRegular14 = TextStyle(
+    color: Color(0xffAAAAAA),
+    fontFamily: 'Montserrat',
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle styleSemiBold18 = TextStyle(
+    color: Colors.white,
+    fontFamily: 'Montserrat',
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle styleBold16 = TextStyle(
+    color: Color(0xff4EB7F2),
+    fontFamily: 'Montserrat',
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle styleMedium20 = TextStyle(
+    color: Colors.white,
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+  );
+}
