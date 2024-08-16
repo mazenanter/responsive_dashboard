6411a27c7f2e57081cf8230e90c3c242099c2e29
MaZen <MazenAnter>
2024-08-17 00:10:39 +0300

    add logic to active and inActive drawer item

---
+++ lib/widgets/active_and_inActive_widgets.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:responsive_dashboard/models/drawer_item_model.dart';
+import 'package:responsive_dashboard/utils/app_styles.dart';
+
+class InActiveDrawerItem extends StatelessWidget {
+  const InActiveDrawerItem({
+    super.key,
+    required this.drawerItemModel,
+  });
+
+  final DrawerItemModel drawerItemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        drawerItemModel.title,
+        style: AppStyles.styleRegular16,
+      ),
+      leading: SvgPicture.asset(drawerItemModel.image),
+    );
+  }
+}
+
+class ActiveDrawerItem extends StatelessWidget {
+  const ActiveDrawerItem({
+    super.key,
+    required this.drawerItemModel,
+  });
+
+  final DrawerItemModel drawerItemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        drawerItemModel.title,
+        style: AppStyles.styleBold16,
+      ),
+      leading: SvgPicture.asset(drawerItemModel.image),
+      trailing: Container(
+        width: 3.27,
+        color: const Color(0xff4EB7F2),
+      ),
+    );
+  }
+}
--- lib/widgets/drawer_item_list_view.dart
+++ lib/widgets/drawer_item_list_view.dart
@@ -3,27 +3,45 @@ import 'package:responsive_dashboard/models/drawer_item_model.dart';
 import 'package:responsive_dashboard/utils/app_images.dart';
 import 'package:responsive_dashboard/widgets/drawer_item.dart';
 
-class DrawerItemListView extends StatelessWidget {
+class DrawerItemListView extends StatefulWidget {
   const DrawerItemListView({
     super.key,
   });
 
-  static const List<DrawerItemModel> drawerItemList = [
-    DrawerItemModel(title: 'Dashboard', image: Assets.imagesCategory2),
-    DrawerItemModel(title: 'My Transaction', image: Assets.imagesConvertCard),
-    DrawerItemModel(title: 'Statistics', image: Assets.imagesGraph),
-    DrawerItemModel(title: 'Wallet Account', image: Assets.imagesWallet2),
-    DrawerItemModel(title: 'My Investments', image: Assets.imagesChart2),
-  ];
+  @override
+  State<DrawerItemListView> createState() => _DrawerItemListViewState();
+}
 
+class _DrawerItemListViewState extends State<DrawerItemListView> {
+  final List<DrawerItemModel> drawerItemList = [
+    const DrawerItemModel(title: 'Dashboard', image: Assets.imagesCategory2),
+    const DrawerItemModel(
+        title: 'My Transaction', image: Assets.imagesConvertCard),
+    const DrawerItemModel(title: 'Statistics', image: Assets.imagesGraph),
+    const DrawerItemModel(title: 'Wallet Account', image: Assets.imagesWallet2),
+    const DrawerItemModel(title: 'My Investments', image: Assets.imagesChart2),
+  ];
+  int activeIndex = 0;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
-      itemBuilder: (context, index) => Padding(
-        padding: const EdgeInsets.only(top: 20.0),
-        child: DrawerItem(drawerItemModel: drawerItemList[index]),
+      itemBuilder: (context, index) => GestureDetector(
+        onTap: () {
+          if (activeIndex != index) {
+            setState(() {
+              activeIndex = index;
+            });
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: DrawerItem(
+            drawerItemModel: drawerItemList[index],
+            isActive: activeIndex == index,
+          ),
+        ),
       ),
       itemCount: drawerItemList.length,
     );
